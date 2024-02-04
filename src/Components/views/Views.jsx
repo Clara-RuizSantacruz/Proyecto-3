c36ca72631302b80bf22cab02065aaca93505d00
Clara Ruiz-Santacruz Jiménez <clara.ruizsantacruz@gmail.com>
2024-02-04 13:15:53 +0100

    whole proyect

---
+++ src/Components/views/Views.jsx
@@ -0,0 +1,14 @@
+
+import UserList from '../Proyecto-3/UserList'
+
+
+function Views() {
+
+  return ( 
+    <>
+    <UserList/>
+    </>
+  )
+}
+
+export default Views
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+//React
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+//Componentes
+import Views from './Components/views/Views'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <React.StrictMode>
+
+    <Views/>
+
+  </React.StrictMode>
+)
